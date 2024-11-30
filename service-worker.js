d3415c2e7171fd1ac745ebbfd325a469a8885cf8
SidiM2024 <sidimohameda524@gmail.com>
2024-11-30 16:49:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,25 @@
 // service-worker.js
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// يجب أن تكون هذه هي إعدادات Firebase الخاصة بك
+// إعدادات Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDtNky0qQbKDGC45pxuqJ4Tq_AMmXpANwI",
   authDomain: "sawaid-4939e.firebaseapp.com",
   projectId: "sawaid-4939e",
-  storageBucket: "sawaid-4939e.firebasestorage.app",
+  storageBucket: "sawaid-4939e.appspot.com",
   messagingSenderId: "817695160648",
   appId: "1:817695160648:web:f34385ce7ec33c88ba9b7d",
   measurementId: "G-44YGD8B1W9"
 };
 
-// تهيئة Firebase داخل الـ Service Worker
+// تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
+
+// تهيئة Firebase Messaging
 const messaging = firebase.messaging();
 
-// هذه هي الطريقة التي سيتم بها عرض الإشعارات في الخلفية
+// معالجة الإشعارات في الخلفية
 messaging.onBackgroundMessage(function(payload) {
   console.log('Received background message ', payload);
   const notificationTitle = payload.notification.title;
